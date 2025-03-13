76df60145a47a593dcf3ad28b97146452e593370
Gabrielsm88 <162460130+Gabrielsm88@users.noreply.github.com>
2025-03-13 09:32:36 -0300

    Update s5.py

--- s5.py
+++ s5.py
@@ -1,6 +1,6 @@
 #IF ENCADEADO
 #VARIAVEL
-salario = 0.0
+salario = novosal = 0.0
 
 #INPUT
 salario = float(input("Digite o salário atual: R$"))
@@ -8,11 +8,11 @@ salario = float(input("Digite o salário atual: R$"))
 #PROCESSAMENTO
 if(salario >= 500):
     if(salario > 1000):
-        salario = salario + (salario * 0.05)
-        print(f"O novo valor do salário é: R${salario:.2f}")
+        novosal = salario * 0.05
     else:
-        salario = salario + (salario * 0.10) 
-        print(f"O novo valor do salário é: R${salario:.2f}")
+        novosal = salario * 0.10
 else:
-    salario = salario + (salario * 0.15)
-    print(f"O novo valor do salário é: R${salario:.2f}")
+    novosal = salario * 0.15
+    
+#OUTPUT
+print(f"O novo valor do salário é: R${novosal:.2f}")
